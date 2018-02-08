c591c1ba395bd2b36105d9ce94a7d47b92a4eb3f
fa <furkanalaybeg@gmail.com>
2018-02-08 14:52:49 +0300

    Youtube Api References and Google Authentication have completed

---
+++ YoutubeSampleApiApp/YoutubeSampleApiApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YoutubeSampleApiApp.Startup))]
+namespace YoutubeSampleApiApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
