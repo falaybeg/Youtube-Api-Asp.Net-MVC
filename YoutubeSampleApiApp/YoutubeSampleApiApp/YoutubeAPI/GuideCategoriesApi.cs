90401c6c7cdb688fd293dad5481baf859ed6f301
fa <furkanalaybeg@gmail.com>
2018-02-14 16:27:33 +0300

    VideoCategories and GuideCategories have been completed

--- YoutubeSampleApiApp/YoutubeSampleApiApp/YoutubeAPI/GuideCategoriesApi.cs
+++ YoutubeSampleApiApp/YoutubeSampleApiApp/YoutubeAPI/GuideCategoriesApi.cs
@@ -1,4 +1,5 @@
 ﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,30 @@ namespace YoutubeSampleApiApp.YoutubeAPI
     public class GuideCategoriesApi
     {
         private static YouTubeService youtubeService = YoutubeApi.Auth();
+
+        public GuideCategory ListVideoCategories()
+        {
+            GuideCategory guideCategory = null;
+            var listRequest = youtubeService.GuideCategories.List("snippet");
+            listRequest.RegionCode = "US";
+
+            var response = listRequest.Execute();
+            if (response.Items.Count > 0)
+            {
+                foreach (var item in response.Items)
+                {
+                    guideCategory = new  GuideCategory();
+                    guideCategory.Snippet = new  GuideCategorySnippet();
+                    guideCategory.Snippet.ChannelId = response.Items[0].Snippet.ChannelId;
+                    guideCategory.Snippet.Title = response.Items[0].Snippet.Title;
+                }
+            }
+            else
+            {
+                throw new Exception("Guide Categories not found");
+            }
+            return guideCategory;
+        }
 
     }
 }
---
+++ YoutubeSampleApiApp/YoutubeSampleApiApp/YoutubeAPI/VideoCategoriesApi.cs
@@ -0,0 +1,44 @@
+﻿using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using YoutubeSampleApiApp.Youtube_API;
+
+namespace YoutubeSampleApiApp.YoutubeAPI
+{
+    public class VideoCategoriesApi
+    {
+        private static YouTubeService youtubeService = YoutubeApi.Auth();
+
+        public VideoCategory ListVideoCategories()
+        {
+            VideoCategory videoCategory = null;
+            var listRequest = youtubeService.VideoCategories.List("snippet");
+            listRequest.RegionCode = "US";
+
+            var response = listRequest.Execute();
+            if (response.Items.Count > 0)
+            {
+                foreach (var item in response.Items)
+                {
+                    videoCategory = new VideoCategory();
+                    videoCategory.Snippet = new VideoCategorySnippet();
+                    videoCategory.Snippet.ChannelId = response.Items[0].Snippet.ChannelId;
+                    videoCategory.Snippet.Title = response.Items[0].Snippet.Title;
+                    videoCategory.Snippet.Assignable = response.Items[0].Snippet.Assignable;
+                }
+            }
+            else
+            {
+                throw new Exception("Categories not found");
+            }
+
+
+            return videoCategory;
+
+        }
+    }
+}
